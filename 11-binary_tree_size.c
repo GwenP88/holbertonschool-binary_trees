e602d4d9368f7f16b4d680c73a5ddc2fe5591d75
GwenP88 <12777@holbertonstudents.com>
2025-12-08 16:14:27 +0100

    add comment task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,11 +12,15 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size_left, size_right;
 
+	/* si l'arbre est vide, sa taille est 0 */
 	if (tree == NULL)
 		return (0);
 
+	/* taille du sous-arbre gauche */
 	size_left = binary_tree_size(tree->left);
+	/* taille du sous-arbre droit */
 	size_right = binary_tree_size(tree->right);
 
+	/* taille totale = 1 (nœud courant) + taille gauche + taille droite */
 	return (1 + size_left + size_right);
 }
