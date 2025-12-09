1212e1b551f04ae511088a2bf655df77bf3486e0
Ali731-Amb <12252@holbertonstudents.com>
2025-12-09 13:29:30 +0100

    Task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,3 +33,30 @@ return (0);
 return (binary_tree_is_perfect(tree->left) &&
 		binary_tree_is_perfect(tree->right));
 }
+
+/**
+ * binary_tree_height - function that measures
+ * the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to traverse
+ * Return: the size of the tree
+ * If tree is NULL return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left, height_right, tree_height;
+
+	if (tree == NULL)
+		return (0);
+
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	if (height_left > height_right)
+		tree_height = height_left;
+	else
+		tree_height = height_right;
+
+	return (1 + tree_height);
+}
+
